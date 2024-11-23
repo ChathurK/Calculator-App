a8be2b5cfa3f7c402bc97356dec27075d4d8e050
ChathurK <kumaras-im21001@stu.kln.ac.lk>
2024-11-23 05:50:18 +0530

    Enhance button component with animated border radius on press

--- components/button.js
+++ components/button.js
@@ -1,17 +1,47 @@
+// useRef is used to store a variable reference to a value.
+import React, { useRef } from "react";
 // Dimentions used to access the screen width and height which helps in calculating responsive layouts.
-// StyleSheet define and manage styles in React Native.
+// StyleSheet define and manage styles.
 // Text is used to render text in the app.
 // TouchableOpacity is a wrapper which gives visual feedback when tapped on an element.
-import React from "react";
-import { Dimensions, StyleSheet, Text, TouchableOpacity } from "react-native";
+// Animated is used to create animations.
+// Easing is used to define the speed of the animation.
+import { Dimensions, StyleSheet, Text, TouchableOpacity, Animated, Easing } from "react-native";
 import { myColors } from "../styles/colors";
 
 // Three properties are defined that are needed to make the button component.
     // 'onPress' is a function that is called when the button is pressed.
     // 'text' is the label that is displayed on the button.
     // 'theme' is used to change the button's appearance. 
-export default ({ onPress, text, theme }) => {
-    const buttonStyles = [styles.button]; // These arrays collect base styles and dynamically append additional styles to the button and text based on the properties.
+const Button = ({ onPress, text, theme }) => {
+    // useRef is used to store the reference of the animated value.
+    // Animated.Value is initialized to half of the button's width.
+    const borderRadius = useRef(new Animated.Value(buttonWidth / 2)).current;
+    
+    /* animation for the button when pressed: when pressed, border radius changes to a quaeter of the button width over 200 milliseconds
+    with a linear easing function. The animation is run on the native thread for better performance. */
+    const handlePressIn = () => {
+        Animated.timing(borderRadius, {
+            toValue: buttonWidth / 4,
+            duration: 200,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    // animation for the button when released: when released, border radius changes back to the original shape.
+    const handlePressOut = () => {
+        Animated.timing(borderRadius, {
+            toValue: buttonWidth / 2,
+            duration: 200,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        }).start();
+    };
+    
+    /* These arrays collect base styles and dynamically append additional styles to the button and text based on the properties
+    to change their appearance in real-time. */
+    const buttonStyles = [styles.button, { borderRadius }];
     const textStyles = [styles.text];
 
     // Logic for theme based styling
@@ -31,7 +61,11 @@ export default ({ onPress, text, theme }) => {
 
     // Return the button element created using TouchableOpacity wrapper that responds when pressed.(dimming effect)
     return (
-        <TouchableOpacity onPress={onPress} style={buttonStyles}>
+        <TouchableOpacity
+            onPress={onPress}
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+            style={buttonStyles}>
             <Text style={textStyles}>{text}</Text>
         </TouchableOpacity>
     );
@@ -39,7 +73,7 @@ export default ({ onPress, text, theme }) => {
 
 
 // set dimmenstion
-const screen = Dimensions.get("window");
+const screen = Dimensions.get("window"); // Get the screen dimensions.
 const buttonWidth = screen.width / 4; // The button width is set as one-fourth of the screen width.
 
 // Define the styles for buttons and texts.
@@ -47,11 +81,13 @@ const styles = StyleSheet.create({
     // base style for buttons
     button: {
         backgroundColor: myColors.primary,
-        flex: 1,
-        height: Math.floor(buttonWidth - 10),
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: Math.floor(buttonWidth),
+        flex: 1,    // this property defines how the button elements are going to fill over the availale space.(space will be devided according to the flex value)
+        height: Math.floor(buttonWidth - 10),   // set the height of the button to be slightly less that the width value to ensure perfect circle.
+                                                //  heigth is 10 units less than the width because of the margin of 5.
+                                                // floor returns the nearest integer that is less than or equal to the real number.
+        alignItems: "center",   // center the content of the button horizontally.
+        justifyContent: "center",   // center the content of the button vertically.
+        borderRadius: Math.floor(buttonWidth / 2),  // set the border radius of the button to the half of the width ensuring rounded corners.
         margin: 5,
     },
 
@@ -81,3 +117,5 @@ const styles = StyleSheet.create({
         backgroundColor: myColors.accentGreen,
     },
 });
+
+export default Button;
--- components/row.js
+++ components/row.js
@@ -1,13 +1,16 @@
 import { StyleSheet, View } from "react-native";
 
+// 'Row' component is a functional component that takes a signle property 'children'
+// nested 'children' components will be displayed inside the 'Row' component.
+// 'View' act as a container for the 'children' components. 
 const Row = ({ children }) => {
     return <View style={styles.container}>{children}</View>;
 };
 
-// create styles of Row
+// define styles of Row
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
+        flexDirection: "row", // Child components are arranged horizontally from left to right.
     },
 });
 
