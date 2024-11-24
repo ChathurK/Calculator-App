24d3e6b5bbdfa1cdd7832505ccfbc55d0ade1e92
ChathurK <kumaras-im21001@stu.kln.ac.lk>
2024-11-25 03:08:05 +0530

    Refactor App component to use icon names for buttons and improve layout structure

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+/* IM/2021/001 - Kumarasingha K.A.C. */
+
 import React, { Component } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
@@ -18,61 +20,80 @@ export default class App extends Component {
   // render method
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.window}>
         <StatusBar style="auto" />
-        <SafeAreaView>
-          <Text style={styles.value}>
-            {parseFloat(this.state.currentValue).toLocaleString()}
-          </Text>
-
-          {/* Do create componentRow */}
+          <View style={styles.textArea}>
+            <Text style={styles.value}>
+              {parseFloat(this.state.currentValue).toLocaleString()}
+            </Text>
+          </View>
+          
+        <View style={styles.buttonArea}>
           <Row>
-            <Button text="AC" theme="accentBlue" onPress={() => this.HandleTap("clear")} />
-            <Button text="+/-" theme="secondary" onPress={() => this.HandleTap("posneg")} />
-            <Button text="%" theme="secondary" onPress={() => this.HandleTap("percentage")} />
-            <Button text="/" theme="secondary" onPress={() => this.HandleTap("operator", "/")} />
+            <Button iconName="square-root" theme="extra" onPress={() => this.HandleTap("operator", "sqrt")} />
+            <Button text="π" theme="extra" onPress={() => this.HandleTap("operator", "pi")} />
+            <Button iconName="exponent" theme="extra" onPress={() => this.HandleTap("operator", "^")} />
+            <Button iconName="exclamation" theme="extra" onPress={() => this.HandleTap("operator", "!")} />
           </Row>
 
-          {/* Number */}
+          <Row>
+            <Button text="AC" theme="accentBlue" onPress={() => this.HandleTap("clear")} />
+            <Button iconName="plus-minus" theme="secondary" onPress={() => this.HandleTap("posneg")} />
+            <Button text="%" theme="secondary" onPress={() => this.HandleTap("percentage")} />
+            <Button iconName="division" theme="secondary" onPress={() => this.HandleTap("operator", "/")} />
+          </Row>
+
           <Row>
             <Button text="7" theme={"primary"} onPress={() => this.HandleTap("number", 7)} />
             <Button text="8" theme={"primary"} onPress={() => this.HandleTap("number", 8)} />
             <Button text="9" theme={"primary"} onPress={() => this.HandleTap("number", 9)} />
-            <Button text="X" theme="secondary" onPress={() => this.HandleTap("operator", "*")} />
+            <Button iconName="multiplication" theme="secondary" onPress={() => this.HandleTap("operator", "*")} />
           </Row>
 
           <Row>
             <Button text="4" theme={"primary"} onPress={() => this.HandleTap("number", 4)} />
             <Button text="5" theme={"primary"} onPress={() => this.HandleTap("number", 5)} />
             <Button text="6" theme={"primary"} onPress={() => this.HandleTap("number", 6)} />
-            <Button text="-" theme="secondary" onPress={() => this.HandleTap("operator", "-")} />
+            <Button iconName="minus" theme="secondary" onPress={() => this.HandleTap("operator", "-")} />
           </Row>
 
           <Row>
             <Button text="1" theme={"primary"} onPress={() => this.HandleTap("number", 1)} />
             <Button text="2" theme={"primary"} onPress={() => this.HandleTap("number", 2)} />
             <Button text="3" theme={"primary"} onPress={() => this.HandleTap("number", 3)} />
-            <Button text="+" theme="secondary" onPress={() => this.HandleTap("operator", "+")} />
+            <Button iconName="plus" theme="secondary" onPress={() => this.HandleTap("operator", "+")} />
           </Row>
 
           <Row>
             <Button text="0" theme={"primary"} onPress={() => this.HandleTap("number", 0)} />
-            <Button text="." theme={"primary"} onPress={() => this.HandleTap("number", ".")} />
-            <Button text="c" theme={"primary"} onPress={() => this.HandleTap("backspace")} />
-            <Button text="=" theme="accentGreen" onPress={() => this.HandleTap("equal", "=")} />
+            <Button iconName="circle-small" theme={"primary"} onPress={() => this.HandleTap("number", ".")} />
+            <Button iconName="backspace" theme={"primary"} onPress={() => this.HandleTap("backspace")} />
+            <Button iconName="equal" theme="accentGreen" onPress={() => this.HandleTap("equal", "=")} />
           </Row>
-        </SafeAreaView>
-      </View>
+        </View>
+        
+      </SafeAreaView>
     );
   }
 }
 
 // create styles of app
 const styles = StyleSheet.create({
-  container: {
+  window: {
     flex: 1,
+    backgroundColor: myColors.primary,
+  },
+  textArea: {
+    display: "flex",
+    backgroundColor: myColors.primary,
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  buttonArea: {
+    borderRadius: 20,
     backgroundColor: myColors.background,
     justifyContent: "flex-end",
+    padding: 5,
   },
   value: {
     color: myColors.textSecondary,
